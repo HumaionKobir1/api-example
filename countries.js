c14f62a1ea5892da185e20bba571e03db4da1c1e
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-25 15:28:29 +0600

    International travel, display country name, capital

---
+++ countries.js
@@ -0,0 +1,24 @@
+const loadCountries = () =>  {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCounties(data));
+}
+
+const displayCounties = countries => {
+    const countriesContainer = document.getElementById('all-countries');
+    // for(const country of countries){
+    //     console.log(country);
+    // }
+    countries.forEach(country =>{
+        console.log(country.name.common);
+
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('country');
+        countryDiv.innerHTML = `
+            <h3>Name: ${country.name.common}</h3>
+            <p>Capital: ${country.capital ? country.capital[0] : 'No Capital'}
+        `;
+        countriesContainer.appendChild(countryDiv);
+    })
+}
+loadCountries();
