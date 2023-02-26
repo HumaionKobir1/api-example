e1364ddb371d12073fdb66a4febd2a4487d2fd46
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-26 13:13:03 +0600

    Added => create daynamic url based on click and display modal

--- meal.js
+++ meal.js
@@ -1,5 +1,6 @@
 const loadMeals = (searchText) => {
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`
+    console.log(url);
     fetch(url)
     .then(res => res.json())
     .then(data => displayMeals(data.meals));
@@ -9,6 +10,7 @@ const displayMeals = meals => {
     const mealsContainer = document.getElementById('meals-container');
     mealsContainer.innerHTML = '';
     meals.forEach(meal => {
+        // console.log(meal);
         const mealDiv = document.createElement('div');
         console.log(meal);
         mealDiv.classList.add('col');
@@ -19,17 +21,38 @@ const displayMeals = meals => {
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+                Details
+            </button>
             </div>
         </div>
         `;
         mealsContainer.appendChild(mealDiv);
-        
     })
 }
+
 
 const searchMeals = () => {
     const searchText = document.getElementById('search-field').value;
     // console.log(searchText);
     loadMeals(searchText);
 }
-loadMeals();
+
+
+const loadMealDetail = idMeal => {
+    const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    console.log(url)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = meal => {
+    console.log(meal.strMeal);
+    document.getElementById('mealDetailsLabel').innerText = meal.strMeal;
+    const mealDetails = document.getElementById('mealDetailsBody');
+    mealDetails.innerHTML = `
+        <img class="img-fluid" src="${meal.strMealThumb}">
+    `
+}
+loadMeals('fish');
