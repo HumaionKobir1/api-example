f65e2982a08ced2066ea60b7bbcdcce1d5c5dd61
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-02-26 14:46:21 +0600

    handle featch error, use try catch an asynce await

--- meal.js
+++ meal.js
@@ -21,7 +21,7 @@ const displayMeals = meals => {
             <div class="card-body">
             <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <button onclick="loadMealDetail(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
+    <button onclick="loadMealDetails(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#mealDetails">
                 Details
             </button>
             </div>
@@ -39,13 +39,38 @@ const searchMeals = () => {
 }
 
 
-const loadMealDetail = idMeal => {
+const loadMealDetails = idMeal => {
     const url = (`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
     console.log(url)
     fetch(url)
     .then(res => res.json())
     .then(data => displayMealDetails(data.meals[0]));
 }
+
+// async await
+/*------------------*/
+
+// const loadMealDetails2 = async(idMeal) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displayMealDetails(data.meals[0]);
+// }
+
+// handle error
+/*------------------*/
+
+// const loadMealDetails2 = async(idMeal) => {
+//     const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+//      try{
+//         const res = await fetch(url);
+//         const data = await res.json();
+//         displayMealDetails(data.meals[0]);
+//      }
+//      catch(error){
+//         console.log(error);
+//      }
+// }
 
 const displayMealDetails = meal => {
     console.log(meal.strMeal);
